e27c6fbd2528a12b5d17cc6afd43186fac3e06aa
XuYongzhe-ethz <99412778+XuYongzhe-ethz@users.noreply.github.com>
2022-10-10 10:08:38 +0800

    add template

--- gRPC_server_template.py
+++ gRPC_server_template.py
@@ -2,23 +2,51 @@ import os
 import sys
 import grpc
 
-import dwgFile_pb2_grpc
-import dwgFile_pb2
+import gRPCFileParser_pb2_grpc
+import gRPCFileParser_pb2
 
 from concurrent import futures
 
 # gRPC server的类
-class CADServicer(dwgFile_pb2_grpc.CADParserServicer):
+class CADServicer(gRPCFileParser_pb2_grpc.gRPCFileParserServicer):
     # 从客户端读取DWG文件，存储在本地，并且原封不动的返回DWG文件
     def parseCADFile(self, request, context):
-        print("file received..")
+        # 这里是接收到的文件都字节流
         file_contents = request.file.content
-        file_name = request.metadata.name
-        print("received new file, ", file_name,
-              ", file size: ", len(file_contents))
-        with open('./server_output/output.dwg', 'wb') as file:
-            file.write(request.file.content)
 
-        # 返回原本的DWG文件
-        return dwgFile_pb2.parseCADResult(metadata=dwgFile_pb2.MetaData(
-            name=file_name), file=dwgFile_pb2.File(content=file_contents))
+        # 接收到的文件的文件名
+        file_name = request.metadata.name
+
+        # --------------------------------------------------
+        #                  添加自己的逻辑代码  
+        # --------------------------------------------------
+        # ...
+
+        # --------------------------------------------------
+        #                  替换以下的变量
+        # --------------------------------------------------
+        # 最终文件的内容
+        final_file_content = bytearray([1,2,3,4,5,6,7,8,9,10])
+
+        # 文件名
+        name = "this_is_a_test"
+
+        # 文件类型
+        file_type = "txt"
+
+        # 返回结果
+        return gRPCFileParser_pb2.parseCADResult(metadata=gRPCFileParser_pb2.MetaData(
+            name=file_name, type=file_type), file=gRPCFileParser_pb2.File(content=final_file_content))
+
+# 开启服务器
+def serve():
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+    gRPCFileParser_pb2_grpc.add_gRPCFileParserServicer_to_server(
+        CADServicer(), server)
+    server.add_insecure_port('[::]:50051')
+    server.start()
+    print("CAD parser is listening in port 50051")
+    server.wait_for_termination()
+
+if __name__ == '__main__':
+    serve()
